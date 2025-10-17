f3b37db4d7de2a44c0fd17a725ef0b90898717e6
Misael-Farias72 <isthatasupra199@gmail.com>
2025-10-17 19:06:11 -0300

    Initial commit

---
+++ RDscript.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("Conv_form")
+const amount = document.getElementById("amount")
+const fromCurrency = document.getElementById("fromCurrency")
+const ConvertedAmount = document.getElementById("ConvertedAmount")
+const toCurrency = document.getElementById("toCurrency")
+const button = document.getElementById("Conv_Button")
+const loading = document.querySelector(".loading")
+const result = document.querySelector(".result")
+const error = document.querySelector(".error")
+const API_URL = "https://api.exchangerate-api.com/v4/latest/"
+
+
+
+async function convertMoney() {
+    /*O css foi chamado para alterar o display do loading de none para block*/
+    loading.style.display = "block"
+    error.style.display = "none"
+    result.style.display = "none"
+
+    /*Tenta acessar o servidor*/
+    try {
+        const response = await fetch(API_URL + fromCurrency.value)
+        const data = await response.json()
+
+        const rate = data.rates[toCurrency.value]
+        const convertedValue = (amount.value * rate).toFixed(3)
+
+        ConvertedAmount.value = convertedValue
+        result.style.display = "block"
+
+        result.innerHTML =
+                `<div style="font-size: 1.4rem;">
+                        ${amount.value} ${fromCurrency.value} = ${ConvertedAmount.value} ${toCurrency.value}
+                  </div>
+                  <div style="font-size: 0.7rem; opacity: 0.8;">
+                       Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
+                  </div>`
+
+    /*Serve para caso ocorra um erro ao acessar o servidor*/
+    } catch (err) {
+        error.style.display = "block"
+        error.innerHTML = "Ocorreu um erro ao acessar o servidor"
+
+
+    }
+
+    loading.style.display = "none"
+}
+
+
+addEventListener("submit", (e) => {
+    e.preventDefault()
+    convertMoney()
+})
